456018996a5c4bb1ea4823de425750f6a46e4792
N-Truong-3012 <3012nhattruong@gamil.com>
2022-04-04 16:32:06 +0700

    week6 bo sung

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/',[MyController::class,'index']);
+Route::get('/{id}',[MyController::class,'goto']);
